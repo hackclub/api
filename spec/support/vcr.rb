01fe236c06e29932a197a4e91f2debc21f59fa32
Zach Latta <zach@zachlatta.com>
2016-10-03 16:57:13 -0500

    Fix code coverage report sending

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,4 +2,7 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join("spec", "vcr")
   c.hook_into :webmock
   c.configure_rspec_metadata!
+
+  # Allow code coverage report to be sent to Code Climate
+  c.ignore_hosts "codeclimate.com"
 end
