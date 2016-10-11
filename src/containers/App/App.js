14b4fa108e5cb018a3a297d162e29a19631b1751
Zach Latta <zach@zachlatta.com>
2016-10-11 08:13:18 -0400

    Fix 404 centering issue

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import config from '../../config'
 
+const styles = {
+  height: '100%'
+}
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={styles}>
         <Helmet {...config.app.head} />
         {this.props.children}
       </div>
