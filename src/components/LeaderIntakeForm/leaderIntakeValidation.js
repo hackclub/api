d3d7cfae4e0fa3edb784f68c41eae55fac267507
Zach Latta <zach@zachlatta.com>
2016-10-15 23:27:06 -0400

    Add validations for gender and year

--- src/components/LeaderIntakeForm/leaderIntakeValidation.js
+++ src/components/LeaderIntakeForm/leaderIntakeValidation.js
@@ -3,6 +3,8 @@ import { createValidator, required, email } from '../../utils/validation'
 export default createValidator({
   name: [required],
   email: [required, email],
+  gender: [required],
+  year: [required],
   phone_number: [required],
   slack_username: [required],
   github_username: [required],
