a9175c580649d46082c07c4092634a7497e247d3
Zach Latta <zach@zachlatta.com>
2017-01-01 11:20:35 -0800

    Fix formatting of Hackbot::Dispatcher

--- app/models/hackbot/dispatcher.rb
+++ app/models/hackbot/dispatcher.rb
@@ -1,24 +1,31 @@
-class Hackbot::Dispatcher
-  CONVERSATION_TYPES = [
-    Hackbot::Conversations::Attendance,
-    Hackbot::Conversations::Mention
-  ].freeze
+module Hackbot
+  class Dispatcher
+    CONVERSATION_TYPES = [
+      Hackbot::Conversations::Attendance,
+      Hackbot::Conversations::Mention
+    ].freeze
 
-  def handle(event, slack_team)
-    wranglers = convos_needing_handling(event)
+    def handle(event, slack_team)
+      wranglers = convos_needing_handling(event)
 
-    return wranglers.each { |w| w.handle(event); w.save! } unless wranglers.empty?
+      return wranglers.each { |w| run_convo(w, event) } unless wranglers.empty?
 
-    to_create = CONVERSATION_TYPES.select { |c| c.should_start? event }
-    created = to_create.map { |c| c.new(team: slack_team) }
+      to_create = CONVERSATION_TYPES.select { |c| c.should_start? event }
+      created = to_create.map { |c| c.new(team: slack_team) }
 
-    created.map { |c| c.handle(event); c.save! }
-  end
+      created.each { |c| run_convo(c, event) }
+    end
 
-  protected
+    private
 
-  def convos_needing_handling(event)
-    Hackbot::Conversation.where.not(state: :finish)
-                         .select { |c| c.part_of_convo? event }
+    def run_convo(convo, event)
+      convo.handle(event)
+      convo.save!
+    end
+
+    def convos_needing_handling(event)
+      Hackbot::Conversation.where.not(state: :finish)
+                           .select { |c| c.part_of_convo? event }
+    end
   end
 end
