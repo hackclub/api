23cc93ca903352c5ad655a70336b40c34de4ac76
Zach Latta <zach@zachlatta.com>
2017-01-01 10:26:04 -0800

    Properly line wrap LeaderSerializer

--- app/serializers/leader_serializer.rb
+++ app/serializers/leader_serializer.rb
@@ -1,6 +1,7 @@
 class LeaderSerializer < ActiveModel::Serializer
   attributes :name, :email, :gender, :year, :phone_number, :slack_username,
-             :github_username, :twitter_username, :address, :latitude, :longitude
+             :github_username, :twitter_username, :address, :latitude,
+             :longitude
 
   has_many :clubs
 end
