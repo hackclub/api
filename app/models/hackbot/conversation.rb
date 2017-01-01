9cf105150937789bd060f63cd53dc6532ab3c78a
Zach Latta <zach@zachlatta.com>
2017-01-01 10:49:31 -0800

    Use nested class definition for Hackbot::Conversation

--- app/models/hackbot/conversation.rb
+++ app/models/hackbot/conversation.rb
@@ -1,52 +1,56 @@
-class Hackbot::Conversation < ApplicationRecord
-  after_initialize :default_values
+module Hackbot
+  class Conversation < ApplicationRecord
+    after_initialize :default_values
 
-  belongs_to :team, foreign_key: 'hackbot_team_id', class_name: ::Hackbot::Team
+    belongs_to :team,
+               foreign_key: 'hackbot_team_id',
+               class_name: ::Hackbot::Team
 
-  validates :team, :state, presence: true
+    validates :team, :state, presence: true
 
-  def self.should_start?(_event)
-    raise NotImplementedError
-  end
+    def self.should_start?(_event)
+      raise NotImplementedError
+    end
 
-  def part_of_convo?(event)
-    # Don't react to events that we cause
-    event[:user] != team.bot_user_id
-  end
+    def part_of_convo?(event)
+      # Don't react to events that we cause
+      event[:user] != team.bot_user_id
+    end
 
-  def handle(event)
-    next_state = send(state, event)
+    def handle(event)
+      next_state = send(state, event)
 
-    self.state = if next_state.is_a? Symbol
-                   next_state
-                 else
-                   :finish
-                 end
+      self.state = if next_state.is_a? Symbol
+                     next_state
+                   else
+                     :finish
+                   end
 
-    send(:finish, event) if state == :finish
-  end
+      send(:finish, event) if state == :finish
+    end
 
-  protected
+    protected
 
-  def start(_event)
-    raise notImplementedError
-  end
+    def start(_event)
+      raise notImplementedError
+    end
 
-  def finish(event); end
+    def finish(event); end
 
-  def send_msg(channel, text)
-    ::SlackClient::Chat.send_msg(
-      channel,
-      text,
-      team.bot_access_token,
-      as_user: true
-    )
-  end
+    def send_msg(channel, text)
+      ::SlackClient::Chat.send_msg(
+        channel,
+        text,
+        team.bot_access_token,
+        as_user: true
+      )
+    end
 
-  private
+    private
 
-  def default_values
-    self.state ||= :start
-    self.data ||= {}
+    def default_values
+      self.state ||= :start
+      self.data ||= {}
+    end
   end
 end
