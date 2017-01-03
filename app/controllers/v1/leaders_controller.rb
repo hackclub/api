fcbb8a7cf0001948fb3fa0390ae2e2cb315bd63c
Zach Latta <zach@zachlatta.com>
2017-01-03 05:00:14 -0800

    Refactor V1::LeadersController

--- app/controllers/v1/leaders_controller.rb
+++ app/controllers/v1/leaders_controller.rb
@@ -1,31 +1,45 @@
-class V1::LeadersController < ApplicationController
-  def intake
-    if params[:club_id].nil?
-      return render json: { errors: { club_id: ["can't be blank"] } }, status: 422
+module V1
+  class LeadersController < ApplicationController
+    before_action :verify_club_id, only: :intake
+
+    def intake
+      leader = Leader.new(leader_params.merge(club_ids: [club_id]))
+
+      if leader.save
+        welcome_letter_for_leader(leader).save!
+        render json: leader, status: 201
+      else
+        render json: { errors: leader.errors }, status: 422
+      end
     end
 
-    leader = Leader.new(leader_params.merge(club_ids: [params[:club_id]]))
+    protected
 
-    if leader.save
-      Letter.create!(
+    def club_id
+      params[:club_id]
+    end
+
+    def verify_club_id
+      return unless club_id.nil?
+      render json: { errors: { club_id: ["can't be blank"] } }, status: 422
+    end
+
+    def leader_params
+      params.permit(
+        :name, :email, :gender, :year, :phone_number, :slack_username,
+        :github_username, :twitter_username, :address
+      )
+    end
+
+    def welcome_letter_for_leader(leader)
+      Letter.new(
         name: leader.name,
-        letter_type: '9002', # This is the type for club leaders
-        what_to_send: '9005', # This is the type for welcome letter + 3oz of stickers
+        # This is the type for club leaders
+        letter_type: '9002',
+        # This is the type for welcome letter + 3oz of stickers
+        what_to_send: '9005',
         address: leader.address
       )
-
-      render json: leader, status: 201
-    else
-      render json: { errors: leader.errors }, status: 422
     end
-  end
-
-  protected
-
-  def leader_params
-    params.permit(
-      :name, :email, :gender, :year, :phone_number, :slack_username,
-      :github_username, :twitter_username, :address
-    )
   end
 end
