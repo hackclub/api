5ab07944496372e19c8181ff65b2604ceed01841
Zach Latta <zach@zachlatta.com>
2017-01-03 05:32:40 -0800

    Rewrap comments to 80 characters in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,19 +12,20 @@ require 'action_cable/engine'
 # require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# Require the gems listed in Gemfile, including any gems you've limited to
+# :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Api
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Settings in config/environments/* take precedence over those specified
+    # here. Application configuration should go into files in
+    # config/initializers -- all .rb files in that directory are automatically
+    # loaded.
 
     # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    # Middleware like session, flash, cookies can be added back manually. Skip
+    # views, helpers and assets when generating a new resource.
     config.api_only = true
 
     # Automatically load files from the lib directory
