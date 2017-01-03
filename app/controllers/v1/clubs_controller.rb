30936470a57326e7206b00e9a5678e081d654578
Zach Latta <zach@zachlatta.com>
2017-01-03 04:18:04 -0800

    Improve module syntax in V1::ClubsController

--- app/controllers/v1/clubs_controller.rb
+++ app/controllers/v1/clubs_controller.rb
@@ -1,57 +1,59 @@
-class V1::ClubsController < ApplicationController
-  before_action :find_club, only: [:show, :update, :destroy]
+module V1
+  class ClubsController < ApplicationController
+    before_action :find_club, only: [:show, :update, :destroy]
 
-  def index
-    render json: Club.all
-  end
-
-  def create
-    club = Club.new(club_params)
-
-    if club.save
-      render json: club, status: 201
-    else
-      render json: { errors: club.errors }, status: 422
-    end
-  end
-
-  def show
-    if @club.nil?
-      render(json: { error: 'Club not found' }, status: 404) && return
+    def index
+      render json: Club.all
     end
 
-    render json: @club
-  end
+    def create
+      club = Club.new(club_params)
 
-  def update
-    if @club.nil?
-      render(json: { error: 'Club not found' }, status: 404) && return
+      if club.save
+        render json: club, status: 201
+      else
+        render json: { errors: club.errors }, status: 422
+      end
     end
 
-    if @club.update_attributes(club_params)
-      render json: @club, status: 200
-    else
-      render json: { errors: @club.errors }, status: 422
+    def show
+      if @club.nil?
+        render(json: { error: 'Club not found' }, status: 404) && return
+      end
+
+      render json: @club
     end
-  end
 
-  def destroy
-    if @club
-      @club.destroy
+    def update
+      if @club.nil?
+        render(json: { error: 'Club not found' }, status: 404) && return
+      end
 
-      render json: { status: 'success' }, status: 200
-    else
-      render json: { error: 'Club not found' }, status: 404
+      if @club.update_attributes(club_params)
+        render json: @club, status: 200
+      else
+        render json: { errors: @club.errors }, status: 422
+      end
     end
-  end
 
-  protected
+    def destroy
+      if @club
+        @club.destroy
 
-  def club_params
-    params.permit(:name, :address, :source, :notes)
-  end
+        render json: { status: 'success' }, status: 200
+      else
+        render json: { error: 'Club not found' }, status: 404
+      end
+    end
 
-  def find_club
-    @club = Club.find_by(id: params[:id])
+    protected
+
+    def club_params
+      params.permit(:name, :address, :source, :notes)
+    end
+
+    def find_club
+      @club = Club.find_by(id: params[:id])
+    end
   end
 end
