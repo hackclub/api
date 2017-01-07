e6d7b23388b57ee8124be218ef2fdce797b825e3
Zach Latta <zach@zachlatta.com>
2017-01-07 14:06:15 -0800

    Fix bug preventing creation of CheckIn instances

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -2,5 +2,5 @@ class CheckIn < ApplicationRecord
   belongs_to :club
   belongs_to :leader
 
-  validates :club, :leader, :meeting_date, :attendance, :notes, presence: true
+  validates :club, :leader, :meeting_date, :attendance, presence: true
 end
