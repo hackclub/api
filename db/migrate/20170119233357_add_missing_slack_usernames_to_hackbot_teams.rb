870753a675bc40a07a3491443ea802aeb4ad5088
Zach Latta <zach@zachlatta.com>
2017-01-20 12:34:26 -0700

    Give AddSlackUsername migration a more descriptive name

--- db/migrate/20170119233357_add_missing_slack_usernames_to_hackbot_teams.rb
+++ db/migrate/20170119233357_add_missing_slack_usernames_to_hackbot_teams.rb
@@ -1,4 +1,4 @@
-class AddSlackUsername < ActiveRecord::Migration[5.0]
+class AddMissingSlackUsernamesToHackbotTeams < ActiveRecord::Migration[5.0]
   def up
     Hackbot::Team.all.each do |team|
       info = ::SlackClient::Users.info(team.bot_user_id, team.bot_access_token)
