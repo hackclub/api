4170a344b6f8c41b884c3332d16a54f1eb3abfef
Zach Latta <zach@zachlatta.com>
2017-03-15 18:51:17 -0700

    Make Hackbot::Utterances.name case insensitive

--- app/models/hackbot/utterances.rb
+++ app/models/hackbot/utterances.rb
@@ -3,7 +3,7 @@ module Hackbot
     # Returns a regex that matches @mentions or regular methods for the given
     # Slack team.
     def self.name(team)
-      /(<@#{team.bot_user_id}>|#{team.bot_username})/
+      /(<@#{team.bot_user_id}>|#{team.bot_username})/i
     end
 
     def self.yes
