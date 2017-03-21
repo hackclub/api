5910f4984757628508317e262ddfd2e7e3764726
Harrison Shoebridge <harrison@theshoebridges.com>
2017-03-21 12:09:24 -0700

    Fix streak_key being nil

--- app/jobs/leader_check_in_job.rb
+++ app/jobs/leader_check_in_job.rb
@@ -3,24 +3,27 @@ class LeaderCheckInJob < ApplicationJob
   queue_as :default
 
   def perform(streak_key)
-    slack_id = slack_id_from_streak_key streak_key
+    @streak_key = streak_key
+
     Hackbot::Interactions::CheckIn.trigger(slack_id)
   end
 
   private
 
-  def slack_id_from_streak_key(streak_key)
+  def slack_id
     if leader.nil?
-      raise(Exception, "Leader with streak key not found: '#{streak_key}'")
+      raise(Exception, "Leader with streak key not found: '#{@streak_key}'")
     end
+
     id = leader.slack_id
     if id.blank?
-      raise(Exception, "Slack ID not found for leader: '#{streak_key}'")
+      raise(Exception, "Slack ID not found for leader: '#{@streak_key}'")
     end
+
     id
   end
 
   def leader
-    Leader.find_by(streak_key: streak_key)
+    Leader.find_by(streak_key: @streak_key)
   end
 end
