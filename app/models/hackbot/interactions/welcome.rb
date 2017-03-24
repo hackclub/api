8b7fa97f0a4ab0aabdc2567c8ee76e52b08ef5c7
Harrison Shoebridge <harrison@theshoebridges.com>
2017-03-24 15:30:58 -0700

    Fix welcome conversation

--- app/models/hackbot/interactions/welcome.rb
+++ app/models/hackbot/interactions/welcome.rb
@@ -4,7 +4,22 @@ module Hackbot
       include Concerns::Triggerable
 
       def start
-        msg_channel copy('welcome')
+        first_name = leader.name.split(' ').first
+
+        msg_channel copy('welcome', name: first_name)
+      end
+
+      private
+
+      def leader
+        pipeline_key = Rails.application.secrets.streak_leader_pipeline_key
+        slack_id_field = :'1020'
+
+        @leader_box ||= StreakClient::Box
+                        .all_in_pipeline(pipeline_key)
+                        .find { |b| b[:fields][slack_id_field] == event[:user] }
+
+        @leader ||= Leader.find_by(streak_key: @leader_box[:key])
       end
     end
   end
