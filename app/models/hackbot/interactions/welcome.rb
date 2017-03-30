ec78d02e9604608f2e4ca6e42edd19440c257651
Max Wofford <max@maxwofford.com>
2017-03-30 13:12:40 -0700

    Welcome interaction is LeaderAssociable

--- app/models/hackbot/interactions/welcome.rb
+++ app/models/hackbot/interactions/welcome.rb
@@ -1,25 +1,12 @@
 module Hackbot
   module Interactions
     class Welcome < TextConversation
-      include Concerns::Triggerable
+      include Concerns::Triggerable, Concerns::LeaderAssociable
 
       def start
         first_name = leader.name.split(' ').first
 
         msg_channel copy('welcome', name: first_name)
-      end
-
-      private
-
-      def leader
-        pipeline_key = Rails.application.secrets.streak_leader_pipeline_key
-        slack_id_field = :'1020'
-
-        @leader_box ||= StreakClient::Box
-                        .all_in_pipeline(pipeline_key)
-                        .find { |b| b[:fields][slack_id_field] == event[:user] }
-
-        @leader ||= Leader.find_by(streak_key: @leader_box[:key])
       end
     end
   end
