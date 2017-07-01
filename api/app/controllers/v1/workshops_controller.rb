3375953ba78b961087f97fbf6fdb0de3bc2c6bc1
Harrison Shoebridge <harrison@theshoebridges.com>
2017-06-30 21:31:15 -0700

    Bypass markdown renderer for non markdown files

--- api/app/controllers/v1/workshops_controller.rb
+++ api/app/controllers/v1/workshops_controller.rb
@@ -3,30 +3,60 @@ module V1
     ROOT_URI = 'v1/workshops/'.freeze
     WORKSHOPS_PATH = 'vendor/hackclub'.freeze
 
-    MARKDOWN_RENDERER_OPTIONS = { autolink: true, tables: true }.freeze
+    MARKDOWN_RENDERER_OPTIONS = {
+      autolink: true,
+      tables: true,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      with_toc_data: true,
+      fenced_code_blocks: true 
+    }.freeze
 
     def workshops
-      renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-                                         MARKDOWN_RENDERER_OPTIONS)
-
       response.content_type = 'text/html'
 
-      begin
-        md = IO.read(file_path)
-
-        out = renderer.render(md)
-
-        render plain: out, status: 200
-      rescue Errno::ENOENT
+      if !File.file? file_path
         render plain: 'File not found', status: 404
+
+        return
+      end
+
+      case File.extname(file_path)
+      when ".md"
+        render_markdown
+      else
+        render_file
       end
     end
 
+    private
+
+    def render_markdown
+      md = IO.read(file_path)
+      renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                         MARKDOWN_RENDERER_OPTIONS)
+
+
+      out = renderer.render(md)
+
+      render plain: out, status: 200
+    end
+
+    def render_file
+      response.content_type = MimeMagic.by_path(file_path)
+
+      f = IO.read(file_path)
+
+      render plain: f, status: 200
+    end
+
     def file_path
+      return @file_path if @file_path
+
       uri = request.env['PATH_INFO']
       param = uri.sub(ROOT_URI, '')
 
-      File.join(WORKSHOPS_PATH, param)
+      @file_path = File.join(WORKSHOPS_PATH, param)
     end
   end
 end
