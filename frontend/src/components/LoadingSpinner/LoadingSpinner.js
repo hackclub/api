cacaa98b48fc6dc713d31443e50bd33ef48c0384
Max Wofford <max@maxwofford.com>
2017-07-02 11:19:49 -0700

    Add loading spinner to workshops

---
+++ frontend/src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Radium from 'radium'
+import { Emoji, Text } from '../../components'
+
+const rotateKeyframes = Radium.keyframes({
+  '0%': {transform: 'rotate(0deg)'},
+  '100%': {transform: 'rotate(359deg)'},
+}, 'linear')
+
+const styles = {
+  textAlign: 'center',
+  margin: '0 auto',
+  animationName: rotateKeyframes,
+  animationDuration: '3s',
+  animationTimingFunction: 'linear',
+  animationIterationCount: 'infinite',
+}
+
+class LoadingSpinner extends Component {
+  render() {
+    return(
+      <div style={styles}>
+        <Emoji type="thinking_face" />
+        <Text>Loading...</Text>
+      </div>
+    )
+  }
+}
+
+export default Radium(LoadingSpinner)
