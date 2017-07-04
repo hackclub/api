db5660664b342f9a3a8d5c754a8f3535c9533d42
Harrison Shoebridge <harrison@theshoebridges.com>
2017-07-03 18:08:10 -0700

    Only render markdown as HTML when a .html extension is provided
    This also introduces a WorkshopFileService, to break logic out of the
    controller.

--- api/app/controllers/v1/workshops_controller.rb
+++ api/app/controllers/v1/workshops_controller.rb
@@ -3,58 +3,20 @@ module V1
     ROOT_URI = 'v1/workshops/'.freeze
     WORKSHOPS_PATH = 'vendor/hackclub'.freeze
 
-    MARKDOWN_RENDERER_OPTIONS = {
-      autolink: true,
-      tables: true,
-      no_intra_emphasis: true,
-      strikethrough: true,
-      with_toc_data: true,
-      fenced_code_blocks: true
-    }.freeze
-
     def workshops
-      response.content_type = 'text/html'
-
-      unless File.file? file_path
-        render plain: 'File not found', status: 404
-
-        return
-      end
-
-      if File.extname(file_path) == '.md'
-        render_markdown
-      else
-        render_file
-      end
-    end
-
-    private
-
-    def render_markdown
-      md = IO.read(file_path)
-      renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-                                         MARKDOWN_RENDERER_OPTIONS)
-
-      out = renderer.render(md)
-
-      render plain: out, status: 200
-    end
-
-    def render_file
-      response.content_type = MimeMagic.by_path(file_path)
-
-      f = IO.read(file_path)
-
-      render plain: f, status: 200
-    end
-
-    def file_path
-      return @file_path if @file_path
-
       uri = request.env['PATH_INFO']
-      param = uri.sub(ROOT_URI, '')
+      path = uri.sub(ROOT_URI, '')
 
-      @file_path = File.join(WORKSHOPS_PATH, param)
+      workshops = WorkshopFileService.new(WORKSHOPS_PATH, path)
+
+      begin
+        out = workshops.handle
+
+        response.content_type = workshops.mime
+        render plain: out, status: 200
+      rescue Errno::ENOENT
+        render plain: 'File not found', status: 404
+      end
     end
   end
 end
---
+++ api/app/services/workshop_file_service.rb
@@ -0,0 +1,47 @@
+class WorkshopFileService
+  MARKDOWN_RENDERER_OPTIONS = {
+    autolink: true,
+    tables: true,
+    no_intra_emphasis: true,
+    strikethrough: true,
+    with_toc_data: true,
+    fenced_code_blocks: true
+  }.freeze
+
+  def initialize(root, file_path)
+    @root = root
+    @path = File.join(@root, file_path)
+  end
+
+  def mime
+    MimeMagic.by_path(@path)
+  end
+
+  def handle
+    return markdown if markdown_as_html?
+
+    raise Errno::ENOENT unless File.file? @path
+
+    file
+  end
+
+  private
+
+  def markdown_as_html?
+    md_path = @path.ext('.md')
+
+    File.extname(@path) == '.html' && File.file?(md_path)
+  end
+
+  def markdown
+    md = IO.read(@path.ext('.md'))
+    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       MARKDOWN_RENDERER_OPTIONS)
+
+    renderer.render(md)
+  end
+
+  def file
+    IO.read(@path)
+  end
+end
