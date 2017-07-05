57a6e674361da059606c53e6c7d770156e7740c0
Harrison Shoebridge <harrison@theshoebridges.com>
2017-07-04 20:45:45 -0700

    Add copy from website

--- frontend/src/containers/ApplyPage/ApplyPage.js
+++ frontend/src/containers/ApplyPage/ApplyPage.js
@@ -1,11 +1,28 @@
 import React, { Component} from 'react'
 import {
   HorizontalRule,
+  Heading,
   NavBar,
+  Container,
+  Link
 } from '../../components'
+
+import pattern from '../../styles/pattern.png'
 import { mediaQueries } from '../../styles/common'
 
 const styles = {
+  wrapper: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'center',
+    paddingTop: '25px',
+    paddingBottom: '25px',
+    [mediaQueries.mediumUp]: {
+      backgroundSize: '450px',
+      paddingTop: '50px',
+      paddingBottom: '60px'
+    }
+  },
   hr: {
     width: '80%',
     height: '4px',
@@ -17,7 +34,39 @@ const styles = {
       width: '70%',
       height: '5px',
       marginTop: '-2.5px'
-    }
+    },
+  }
+}
+
+const headingStyles = {
+  fontWeight: 'bolder',
+  fontSize: '26px',
+  [mediaQueries.mediumUp]: {
+    fontSize: '36px'
+  }
+}
+
+class ApplyHeading extends Component {
+  render() {
+    return (
+      <div>
+        <Heading style={headingStyles}>Apply to Start a Hack Club</Heading>
+      </div>
+    )
+  }
+}
+
+class ApplyInfo extends Component {
+  render() {
+    return (
+      <div>
+        <p>We're accepting applications all the time. It's great if your responses to these questions are over 3 sentences long, but make sure you don't write an essay!</p>
+
+        <br />
+
+        <p>Check out our <Link href="/example_applications">example applications</Link> if you want some help!</p>
+      </div>
+    )
   }
 }
 
@@ -26,7 +75,11 @@ class Apply extends Component {
     return (
       <div>
         <NavBar />
-        <h1>Hello</h1>
+        <Container style={[styles.wrapper]}>
+          <ApplyHeading />
+
+          <ApplyInfo />
+        </Container>
       </div>
     )
   }
