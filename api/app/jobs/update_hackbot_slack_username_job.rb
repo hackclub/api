f5948dde71a8d7cad2dd9fa96402defb70667627
Max Wofford <max@maxwofford.com>
2017-07-14 05:49:36 -0700

    Revoking Hackbot from a team no longer raises errors
    This fixes a problem where Hackbot doesn't stop crying after the other kids
    kick it out of their playground.

--- api/app/jobs/update_hackbot_slack_username_job.rb
+++ api/app/jobs/update_hackbot_slack_username_job.rb
@@ -4,7 +4,11 @@ class UpdateHackbotSlackUsernameJob < ApplicationJob
   def perform(*)
     Hackbot::Team.find_each do |team|
       info = ::SlackClient::Users.info(team.bot_user_id, team.bot_access_token)
-      team.update(bot_username: info[:user][:name])
+
+      # https://api.slack.com/methods/users.info
+      unless info[:error] == 'account_inactive'
+        team.update(bot_username: info[:user][:name])
+      end
     end
   end
 end
