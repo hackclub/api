c7bb7b6ff95079823c31c7624376a5b334a0c0be
Max Wofford <max@maxwofford.com>
2017-07-18 16:09:14 -0700

    Add logging for Slack's account_inactive errors

--- api/app/jobs/update_hackbot_slack_username_job.rb
+++ api/app/jobs/update_hackbot_slack_username_job.rb
@@ -6,7 +6,10 @@ class UpdateHackbotSlackUsernameJob < ApplicationJob
       info = ::SlackClient::Users.info(team.bot_user_id, team.bot_access_token)
 
       # https://api.slack.com/methods/users.info
-      unless info[:error] == 'account_inactive'
+      if info[:error] == 'account_inactive'
+        logger.info("Slack returned 'account_inactive' in team " \
+                    "'#{team.team_name}'")
+      else
         team.update(bot_username: info[:user][:name])
       end
     end
