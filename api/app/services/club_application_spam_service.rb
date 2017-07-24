c256f6f7a7a40f043ce46a4dc2e67186fc6495d1
Harrison Shoebridge <harrison@theshoebridges.com>
2017-07-24 00:00:15 -0700

    Fix ClubApplicationSpamService's nil values

--- api/app/services/club_application_spam_service.rb
+++ api/app/services/club_application_spam_service.rb
@@ -2,6 +2,9 @@ class ClubApplicationSpamService
   WORD_CUTOFF = 5
 
   def spam?(application)
+    return false if application.interesting_project.nil? ||
+                    application.systems_hacked.nil?
+
     word_count(application.interesting_project) <= WORD_CUTOFF ||
       word_count(application.systems_hacked) <= WORD_CUTOFF
   end
