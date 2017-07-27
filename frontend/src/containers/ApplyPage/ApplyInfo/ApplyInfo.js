1383f1b586ffd6c1046de369b8de55fa9ba4fee6
Zach Latta <zach@zachlatta.com>
2017-07-27 05:07:37 -0700

    Remove broken link to example applications

--- frontend/src/containers/ApplyPage/ApplyInfo/ApplyInfo.js
+++ frontend/src/containers/ApplyPage/ApplyInfo/ApplyInfo.js
@@ -7,10 +7,6 @@ class ApplyInfo extends Component {
     return (
       <div>
         <p>We're accepting applications all the time. It's great if your responses to these questions are over 3 sentences long, but make sure you don't write an essay!</p>
-
-        <br />
-
-        <p>Check out our <Link href="/example_applications">example applications</Link> if you want some help!</p>
       </div>
     )
   }
