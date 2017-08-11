3435b3833e9c05ceb91c34be624051daf46b65fc
Max Wofford <max@maxwofford.com>
2017-08-11 12:12:03 -0700

    Fix title tag
    This issue was introduced in https://github.com/hackclub/monolith/commit/da2b5b6424f6233e3d2ba88bdd4eaf66f33698f9

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import config from '../../config'
 
 const styles = {
   height: '100%',
@@ -9,6 +11,7 @@ class App extends Component {
   render() {
     return (
       <div style={styles}>
+        <Helmet {...config.head} />
         {this.props.children}
       </div>
     )

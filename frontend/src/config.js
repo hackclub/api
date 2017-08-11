hackclub/api

commit da2b5b6424f6233e3d2ba88bdd4eaf66f33698f9

Fix issue where open graph tags render after page load

frontend/src/containers/App/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import Helmet from 'react-helmet'
-import config from '../../config'
 
 const styles = {
   height: '100%',
@@ -11,7 +9,6 @@ class App extends Component {
   render() {
     return (
       <div style={styles}>
-        <Helmet {...config.app.head} />
         {this.props.children}
       </div>
     )

frontend/public/index.html
@@ -1,11 +1,17 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <meta property="og:title" content="Hack Club" />
+    <meta property="og:description" content="The worldwide movement of student-led coding clubs." />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="%PUBLIC_URL%/og/image.jpg" />
+    <meta property="og:url" content="https://hackclub.com" />
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   </head>
   <body>
     <div id="root"></div>

commit 3435b3833e9c05ceb91c34be624051daf46b65fc

Fix title tag
This issue was introduced in https://github.com/hackclub/monolith/commit/da2b5b6424f6233e3d2ba88bdd4eaf66f33698f9

frontend/src/config.js
@@ -1,25 +1,7 @@
-const info = {
-  title: 'Hack Club',
-  description: 'The worldwide movement of student-led coding clubs.'
-}
-
 export default {
-  app: {
-    title: info.title,
-    description: info.description,
-    head: {
-      titleTemplate: '%s | Hack Club',
-      defaultTitle: info.title,
-      meta: [
-        {name: 'description', content: info.description},
-        {charset: 'utf-8'},
-        {property: 'og:site_name', content: info.title},
-        {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: info.title},
-        {property: 'og:description', content: info.description},
-        {property: 'og:card', content: 'summary'}
-      ]
-    }
+  head: {
+    titleTemplate: '%s | Hack Club',
+    defaultTitle: 'Hack Club'
   },
   apiBaseUrl: process.env.REACT_APP_API_BASE_URL,
   slackClientId: process.env.REACT_APP_SLACK_CLIENT_ID,

frontend/src/containers/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import config from '../../config'
 
 const styles = {
   height: '100%',
@@ -9,6 +11,7 @@ class App extends Component {
   render() {
     return (
       <div style={styles}>
+        <Helmet {...config.head} />
         {this.props.children}
       </div>
     )