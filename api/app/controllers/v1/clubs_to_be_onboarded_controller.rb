24ea58598d4bf6174094301f65e36cb776d008dc
Harrison Shoebridge <harrison@theshoebridges.com>
2017-08-16 14:39:14 -0700

    Fix incorrect key

--- api/app/controllers/v1/clubs_to_be_onboarded_controller.rb
+++ api/app/controllers/v1/clubs_to_be_onboarded_controller.rb
@@ -1,6 +1,6 @@
 module V1
   class ClubsToBeOnboardedController < ApplicationController
-    TO_BE_ONBOARDED_STAGE = '5001'.freeze
+    TO_BE_ONBOARDED_STAGE = '5011'.freeze
 
     def show
       render json: Club.select { |c| c.stage_key == TO_BE_ONBOARDED_STAGE }
