ae4b35c5e59ed622590a806f82f5733a914fbc92
Zach Latta <zach@zachlatta.com>
2017-08-22 20:06:33 -0700

    WIP commit

--- frontend/src/containers/ApplyPage/ApplyInfo/ApplyInfo.js
+++ frontend/src/containers/ApplyPage/ApplyInfo/ApplyInfo.js
@@ -1,16 +1,64 @@
 import React, { Component } from 'react'
-import { Text } from '../../../components'
+import Radium from 'radium'
+import { Card, Text } from '../../../components'
+
+const styles = {
+  textAlign: 'left',
+  maxWidth: '100%'
+}
 
 class ApplyInfo extends Component {
   render() {
     return (
-      <Text>
-        We're accepting applications all the time. It's great if your responses
-        to these questions are over 3 sentences long, but make sure you don't
-        write an essay!
-      </Text>
+      <Card style={styles}>
+        <Text>
+          At this point in Hack Club's history, we've worked with hundreds of
+          high schoolers to start over 200 Hack Clubs.
+        </Text>
+        <Text>
+          One of the things we've learned is that it's very difficult to predict
+          a club's success from the initial leader's application. We've had
+          clubs we nearly rejected succeed beyond any expectation and we've had
+          clubs we were excited about fail miserably.
+        </Text>
+        <Text>
+          The one pattern we have noticed across nearly all clubs is that their
+          success or failure is almost always a result of the club's leadership.
+          The leaders make the club.
+        </Text>
+        <Text>
+          Our best clubs are led by leaders that are gritty. People who will
+          stop at nothing to see their goals through. By people who see
+          challenges and instead of asking "why" ask "why not".
+        </Text>
+        <Text>
+          Our best leaders are thoughtful. They think deeply about the world
+          around them and break things down to their fundamentals. They look for
+          simpler ways to do things, not accepting the status quo.
+        </Text>
+        <Text>
+          Our best leaders are team players and understand it takes more than
+          one person to build an institution at their school.
+        </Text>
+        <Text>
+          Our goal with this application is to get a taste of who you are. Are
+          you gritty? How do you think about the world? What are the deeply
+          personal experiences you've had that have led you to applying today?
+        </Text>
+        <Text>
+          These days, we accept nearly everyone who applies to start a Hack Club
+          because we know how terrible we are at predicting success. But we do
+          want you to take this seriously.
+        </Text>
+        <Text>
+          Your answers to this application will inform how we work with you.
+          They'll tell us what kind of person you are, who at Hack Club will
+          work with you, and how we should best be helping you get your club off
+          the ground.
+        </Text>
+      </Card>
     )
   }
 }
 
-export default ApplyInfo
+export default Radium(ApplyInfo)
