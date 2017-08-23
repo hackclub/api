b4baf17282ebec8fc8d98abe584bcb8ab402c962
Zach Latta <zach@zachlatta.com>
2017-08-22 18:47:01 -0700

    Use Text component in ApplyInfo

--- frontend/src/containers/ApplyPage/ApplyInfo/ApplyInfo.js
+++ frontend/src/containers/ApplyPage/ApplyInfo/ApplyInfo.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Text } from '../../../components'
 
 class ApplyInfo extends Component {
   render() {
     return (
-      <div>
-        <p>We're accepting applications all the time. It's great if your responses to these questions are over 3 sentences long, but make sure you don't write an essay!</p>
-      </div>
+      <Text>
+        We're accepting applications all the time. It's great if your responses
+        to these questions are over 3 sentences long, but make sure you don't
+        write an essay!
+      </Text>
     )
   }
 }
