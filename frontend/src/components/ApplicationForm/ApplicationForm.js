9ea40ce7da2ad95c3d18c8313c6667277fa01e63
Max Wofford <max@maxwofford.com>
2017-08-23 18:24:46 -0700

    Shorter response fields on the application page

--- frontend/src/components/ApplicationForm/ApplicationForm.js
+++ frontend/src/components/ApplicationForm/ApplicationForm.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import Radium from 'radium'
 import { reduxForm, Field } from 'redux-form'
 import { Button, Emoji, Link, TextField, TextAreaField, SelectField } from '../../components'
 import applicationValidation from './applicationValidation'
+import { mediaQueries } from '../../styles/common.js'
 
 const monthNames = [
   'January',
@@ -18,10 +20,24 @@ const monthNames = [
   'December'
 ]
 
-const subtitleStyles = {
-  fontWeight: 300,
-  paddingBottom: '8px',
-  float: 'right'
+const styles = {
+  subtitle: {
+    fontWeight: 300,
+    paddingBottom: '8px',
+    float: 'right',
+  },
+  shortResponseWrapper: {
+      display: 'flex',
+      flexFlow: 'row wrap',
+      alignItems: 'baseline',
+      justifyContent: 'space-between',
+  },
+  shortResponse: {
+    width: '100%',
+    [mediaQueries.mediumUp]: {
+      width: '49%'
+    }
+  }
 }
 
 class Subtitle extends Component {
@@ -30,13 +46,25 @@ class Subtitle extends Component {
       <div>
         <div style={{ height: '10px'}} />
 
-        <p style={subtitleStyles}>{this.props.children}</p>
+        <p style={styles.subtitle}>{this.props.children}</p>
 
         <div style={{float: 'clear'}}/>
       </div>
     )
   }
 }
+
+class ShortResponseField extends Component {
+  render() {
+    return(
+      <div style={styles.shortResponse}>
+        <Field {...this.props} />
+      </div>
+    )
+  }
+}
+// We do this to wrap our component in Radium for our mediaQueries
+ShortResponseField = Radium(ShortResponseField)
 
 class ApplicationForm extends Component {
   buttonState() {
@@ -94,45 +122,43 @@ class ApplicationForm extends Component {
 
     return (
       <form style={style} onSubmit={handleSubmit}>
-        <Subtitle>Information</Subtitle>
+        <div style={styles.shortResponseWrapper}>
+          <ShortResponseField name="first_name" label="First name" placeholder="Fiona" component={TextField} />
+          <ShortResponseField name="last_name" label="Last name" placeholder="Hackworth" component={TextField} isRight />
+          <ShortResponseField name="email" type="email" label="Preferred email" placeholder="fiona@hackclub.com" component={TextField} />
+          <ShortResponseField name="phone_number" label="Phone number" placeholder="(555) 555 5555" component={TextField} isRight />
+          <ShortResponseField name="high_school" label="High school" placeholder="Atlantis High School" component={TextField} />
 
-        <Field name="first_name" label="First name" placeholder="Fiona" component={TextField} />
-        <Field name="last_name" label="Last name" placeholder="Hackworth" component={TextField} />
-        <Field name="email" type="email" label="Preferred email" placeholder="fiona@hackclub.com" component={TextField} />
-        <Field name="phone_number" label="Phone number" placeholder="(555) 555 5555" component={TextField} />
-        <Field name="high_school" label="High school" placeholder="Atlantis High School" component={TextField} />
+          <ShortResponseField name="year" label="When do you graduate high school?" component={SelectField} isRight>
+            <option value="9010">Other</option>
+            <option value="9001">2022</option>
+            <option value="9002">2021</option>
+            <option value="9003">2020</option>
+            <option value="9004">2019</option>
+            <option value="9005">2018</option>
+            <option value="9006">2017</option>
+            <option value="9007">2016</option>
+            <option value="9009">Teacher</option>
+            <option value="9008">Graduated</option>
+          </ShortResponseField>
 
-        <Field name="year" label="When do you graduate high school?" component={SelectField}>
-          <option value="9010">Other</option>
-          <option value="9001">2022</option>
-          <option value="9002">2021</option>
-          <option value="9003">2020</option>
-          <option value="9004">2019</option>
-          <option value="9005">2018</option>
-          <option value="9006">2017</option>
-          <option value="9007">2016</option>
-          <option value="9009">Teacher</option>
-          <option value="9008">Graduated</option>
-        </Field>
+          <ShortResponseField name="github" label="GitHub" placeholder="FionaHackworth" component={TextField} />
+          <ShortResponseField name="twitter" label="Twitter" placeholder="fionahack" component={TextField} isRight />
 
-        <Field name="github" label="GitHub" placeholder="FionaHackworth" component={TextField} />
-        <Field name="twitter" label="Twitter" placeholder="fionahack" component={TextField} />
+          <ShortResponseField name="referer" label="How did you hear about us?" component={TextField} placeholder="" isRight />
 
-        <Field name="start_date" label="When do you want to start your club?" component={SelectField}>
-          { this.next12Months().map(month => {
-            return (<option value={month.iso} key={month.iso}>{month.label}</option>)
-          })}
-        </Field>
-
-        <Subtitle>Application</Subtitle>
+          <ShortResponseField name="start_date" label="When do you want to start your club?" component={SelectField}>
+            { this.next12Months().map(month => {
+              return (<option value={month.iso} key={month.iso}>{month.label}</option>)
+            })}
+          </ShortResponseField>
+        </div>
 
         <Field name="interesting_project" label="Please tell us about an interesting project, preferably outside of class, that you created or worked on." component={TextAreaField} />
 
         <Field name="systems_hacked" label={<span>Please tell us about the time you most successfully hacked some (non-computer) system to your advantage. <Link href="https://www.quora.com/When-have-you-most-successfully-hacked-a-non-computer-system-to-your-advantage">Click here</Link> for the sorts of responses we're looking for.</span>} component={TextAreaField} />
 
         <Field name="steps_taken" label="What steps have you taken so far in starting your club?" component={TextAreaField} />
-
-        <Field name="referer" label="How did you hear about us?" component={TextField} placeholder="" />
 
         <Button type="form"
                 state={this.buttonState()}>{this.buttonText(status)}</Button>
