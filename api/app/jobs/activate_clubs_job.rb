037ab3ec2c3a7e3e331994504bbc861403344feb
Harrison Shoebridge <harrison@theshoebridges.com>
2017-08-25 18:09:49 -0700

    Fix rubocop

--- api/app/jobs/activate_clubs_job.rb
+++ api/app/jobs/activate_clubs_job.rb
@@ -4,6 +4,6 @@ class ActivateClubsJob < ApplicationJob
       .where('activation_date IS NOT NULL')
       .select(&:dormant)
       .select { |c| Time.zone.now > Date.parse(c.activation_date) }
-      .each { |c| c.make_active }
+      .each(&:make_active)
   end
 end
