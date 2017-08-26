e54c249cd6a84999442c9f082f53c3b73b0b6bef
Harrison Shoebridge <harrison@theshoebridges.com>
2017-08-26 12:19:46 -0700

    Fix typo

--- api/app/jobs/activate_clubs_job.rb
+++ api/app/jobs/activate_clubs_job.rb
@@ -2,7 +2,7 @@ class ActivateClubsJob < ApplicationJob
   def perform
     Club
       .where('activation_date IS NOT NULL')
-      .select(&:dormant)
+      .select(&:dormant?)
       .select { |c| Time.zone.now > Date.parse(c.activation_date) }
       .each(&:make_active)
   end
