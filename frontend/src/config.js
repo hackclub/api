0748256ca6979cfa5fb2908bbc76dd6a2dddd543
Max Wofford <max@maxwofford.com>
2017-09-01 13:16:42 -0700

    Stripe publishable key is set for the frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,5 +5,6 @@ export default {
   },
   apiBaseUrl: process.env.REACT_APP_API_BASE_URL,
   segmentAnalyticsKey: process.env.REACT_APP_SEGMENT_ANALYTICS_KEY,
-  slackClientId: process.env.REACT_APP_SLACK_CLIENT_ID
+  slackClientId: process.env.REACT_APP_SLACK_CLIENT_ID,
+  stripePublishableKey: process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 }
