3055b78404c80f998a6a96fcaac7f58e1dfbff40
Max Wofford <max@maxwofford.com>
2017-09-15 13:10:27 -0700

    Fix Segment key env var name

--- web/src/config.js
+++ web/src/config.js
@@ -4,7 +4,7 @@ export default {
     defaultTitle: 'Hack Club'
   },
   apiBaseUrl: process.env.REACT_APP_API_BASE_URL,
-  segmentAnalyticsKey: process.env.REACT_APP_SEGMENT_ANALYTICS_KEY,
+  segmentKey: process.env.REACT_APP_SEGMENT_KEY,
   slackClientId: process.env.REACT_APP_SLACK_CLIENT_ID,
   stripePublishableKey: process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 }
