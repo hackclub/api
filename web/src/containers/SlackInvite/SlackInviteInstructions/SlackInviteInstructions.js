cf33d622a0988a7220c916f550d977e0b0f5630f
Max Wofford <max@maxwofford.com>
2017-09-19 04:36:19 -0700

    Style loading and information pages

--- web/src/containers/SlackInvite/SlackInviteInstructions/SlackInviteInstructions.js
+++ web/src/containers/SlackInvite/SlackInviteInstructions/SlackInviteInstructions.js
@@ -18,6 +18,12 @@ const styles = {
     marginRight: 'auto',
     maxWidth: '700px',
   },
+  email: {
+    fontWeight: 600
+  },
+  text: {
+    marginTop: '16px',
+  },
   image: {
     width: '100%',
     boxShadow: `0px 1px 50px -15px ${colors.gray}`,
@@ -32,13 +38,13 @@ class SlackInviteInstructions extends Component {
     return (
       <Card style={[styles.card,styles.wideCard]}>
         <Button type="form" state="success">Success! <Emoji type="party_popper"/></Button>
-        <Text>We're generating an account on Slack for you. Once we get it set up, we'll transfer it to your account email.</Text>
+        <Text style={styles.text}>We're generating an account on Slack for you. Once we get it set up, we'll transfer it to your account email.</Text>
         <Heading>Here's the next step to join:</Heading>
-        <Text>You'll get an email in the next few minutes from Slack. Click this button:</Text>
+        <Text style={styles.text}>You'll get an email in the next few minutes from Slack. Click this button:</Text>
         <img style={styles.image} src={exampleSlackEmail} alt="Slack email" />
-        <Text>You'll get taken to a page that looks like this:</Text>
+        <Text style={styles.text}>You'll get taken to a page that looks like this:</Text>
         <img style={styles.image} src={slackValidationPage} alt="Slack validation webpage" />
-        <Text>To sign in the first time, you'll need to type in {inviteEmail} as your email.</Text>
+        <Text style={styles.text}>To sign in the first time, you'll need to type in <span style={styles.email}>{inviteEmail}</span> as your email.</Text>
       </Card>
     )
   }
--- web/src/containers/SlackInvite/SlackInviteLoading/SlackInviteLoading.js
+++ web/src/containers/SlackInvite/SlackInviteLoading/SlackInviteLoading.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import { Card, LoadingSpinner } from 'components'
 
 const stages = ['invited', 'invite_received', 'signed_up', 'configured_client', 'changed_email']
+
+const styles = {
+  card: {
+    marginTop: '20px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  loadingSpinner: {
+    marginTop: '3em',
+    marginBottom: '1em',
+  }
+}
 
 class SlackInviteLoading extends Component {
   componentDidMount() {
@@ -16,9 +29,10 @@ class SlackInviteLoading extends Component {
     const stageNumber = stages.indexOf(inviteState) + 1
 
     return (
-      <p>
-        Loading content... Working on step {stageNumber} / {stages.length}...
-      </p>
+      <Card style={styles.card}>
+        <LoadingSpinner style={styles.loadingSpinner} />
+        <p>Working on step {stageNumber} / {stages.length}...</p>
+      </Card>
     )
   }
 }
