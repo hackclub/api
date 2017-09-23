f6472f118659ac988cf0d64d5575228eaaa203a1
Max Wofford <max@maxwofford.com>
2017-09-23 04:05:01 -0700

    Fix-201

--- web/src/containers/SlackInvite/SlackInviteFormWrapper/SlackInviteFormWrapper.js
+++ web/src/containers/SlackInvite/SlackInviteFormWrapper/SlackInviteFormWrapper.js
@@ -8,8 +8,11 @@ import {
 } from 'components'
 
 const styles = {
-  subtitle: {
+  subtitleBottom: {
     marginTop: '15px'
+  },
+  subtitleTop: {
+    marginBottom: '15px'
   },
   card: {
     marginTop: '20px',
@@ -22,11 +25,14 @@ class SlackInviteFormWrapper extends Component {
     const { status, onSubmit } = this.props
     return (
       <Card style={styles.card}>
+        <Subtitle style={styles.subtitleTop}>
+          Already have an account? Go directly to <Link href="//hackclub.slack.com">Slack</Link>.
+        </Subtitle>
         <SlackInviteForm
           status={status}
           onSubmit={onSubmit} />
-        <Subtitle style={styles.subtitle}>
-          Already have an account? Go directly to <Link href="//hackclub.slack.com">Slack</Link>.
+        <Subtitle style={styles.subtitleBottom}>
+          By filling out this form, you are agreeing to Slack's <Link href="https://slack.com/terms-of-service/user">User Terms of Service</Link>, <Link href="https://slack.com/privacy-policy">Privacy Policy</Link>, and <Link href="https://slack.com/cookie-policy">Cookie Policy</Link>.
         </Subtitle>
       </Card>
     )
