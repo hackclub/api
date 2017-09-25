25575a8f53af58ee1d12cd8a8830e7ab5e2ebe38
Harrison Shoebridge <harrison@theshoebridges.com>
2017-09-24 17:39:02 -0700

    Add stubbed endpoint

---
+++ api/app/controllers/v1/slack_invitation/strategies_controller.rb
@@ -0,0 +1,17 @@
+module V1
+  module SlackInvitation
+    class StrategiesController < ApplicationController
+      def show
+        render json:  {
+            id: 0,
+            name: "mason",
+            club_name: "Mason Hack Club",
+            primary_color: "#d31b6b",
+            greeting: "Hello welcome to Hack Club's Slack :)",
+            channels: [],
+            user_groups: []
+          }
+      end
+    end
+  end
+end
