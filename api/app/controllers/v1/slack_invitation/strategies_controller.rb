649800f0bbf2c0b376cef03de39d55e5d9e32b40
Harrison Shoebridge <harrison@theshoebridges.com>
2017-09-25 18:41:34 -0700

    Add proper lookup for strategies

--- api/app/controllers/v1/slack_invitation/strategies_controller.rb
+++ api/app/controllers/v1/slack_invitation/strategies_controller.rb
@@ -2,15 +2,15 @@ module V1
   module SlackInvitation
     class StrategiesController < ApplicationController
       def show
-        render json:  {
-          id: 0,
-          name: 'mason',
-          club_name: 'Mason Hack Club',
-          primary_color: '#d31b6b',
-          greeting: "Hello welcome to Hack Club's Slack :)",
-          channels: [],
-          user_groups: []
-        }
+        @strat = SlackInviteStrategy.find_by(name: params[:id])
+
+        unless @strat
+          render json: {}, status: 404
+
+          return
+        end
+
+        render json: @strat
       end
     end
   end
