9b5ca69aa5875cd49ea9da34bb0ed031970ba4d1
Max Wofford <max@maxwofford.com>
2017-10-23 14:10:36 -0700

    Disable router links

--- web/src/components/Link/Link.js
+++ web/src/components/Link/Link.js
@@ -1,9 +1,11 @@
+// Router links are disabled while the site is transitioning to
+// the lachlan.hackclub.com page.
 import React from 'react'
 import Radium from 'radium'
-import { Link as RouterLink } from 'react-router'
+/* import { Link as RouterLink } from 'react-router'*/
 import colors from 'styles/colors'
 
-const RadiumRouterLink = Radium(RouterLink)
+/* const RadiumRouterLink = Radium(RouterLink)*/
 
 const styles = {
   color: colors.primary,
@@ -20,16 +22,16 @@ const Link = props => {
   // a passed parameter, then an anchor tag is used.
   const { to, href } = props
 
-  if (to) {
-    return (
-      <RadiumRouterLink to={to} style={[styles, props.style]}>
-        {props.children}
-      </RadiumRouterLink>
-    )
-  }
+  /* if (to) {
+   *   return (
+   *     <RadiumRouterLink to={to} style={[styles, props.style]}>
+   *       {props.children}
+   *     </RadiumRouterLink>
+   *   )
+   * }*/
 
   return (
-    <a href={href} style={[styles, props.style]}>
+    <a href={href || to} style={[styles, props.style]}>
       {props.children}
     </a>
   )
