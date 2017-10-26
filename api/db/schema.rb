4d81969b73b2208753e1282090da61d7e5bf796b
Max Wofford <max@maxwofford.com>
2017-10-25 20:16:09 -0700

    Run rails db:migrate

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20171024132156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "admin_users", force: :cascade do |t|
     t.text     "team"
