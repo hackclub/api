baae63d3c3f3d586b93c533e6276219791449b1a
Max Wofford <max@maxwofford.com>
2017-12-13 10:59:28 -0800

    Add finder.hackclub.com to CORs

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   DOMAINS = ['new.hackclub.com',
              'legacy.hackclub.com',
              'lachlan.hackclub.com',
+             'finder.hackclub.com',
              'hackclub.com',
              'bulckcah.com',
              'repl.it'].freeze
