8bf7120b22c3d296d7f1fb31ae841bd8e0aefd21
Zach Latta <zach@zachlatta.com>
2017-12-27 22:36:01 -0800

    Put subject login code in parens

--- api/app/mailers/applicant_mailer.rb
+++ api/app/mailers/applicant_mailer.rb
@@ -2,6 +2,6 @@ class ApplicantMailer < ApplicationMailer
   def login_code(applicant)
     @login_code = applicant.login_code
 
-    mail(to: applicant.email, subject: "Hack Club Login Code #{@login_code}")
+    mail(to: applicant.email, subject: "Hack Club Login Code (#{@login_code})")
   end
 end
