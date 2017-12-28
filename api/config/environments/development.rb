10c9bc6df497897c83bb5e8db9acaec54a97983e
Zach Latta <zach@zachlatta.com>
2017-12-27 22:35:13 -0800

    Use SMTP for dev emails

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -27,7 +27,15 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.secrets.smtp_username,
+    password: Rails.application.secrets.smtp_password,
+    address: Rails.application.secrets.smtp_address,
+    domain: Rails.application.secrets.smtp_domain,
+    port: Rails.application.secrets.smtp_port,
+    authentication: :plain
+  }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
