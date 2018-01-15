42548718bfc7f8c4ce54da4edfc124431f64be7f
Zach Latta <zach@zachlatta.com>
2018-01-15 05:10:44 -0800

    Allow access of Sidekiq dashboard

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   namespace :v1 do
     get 'ping', to: 'ping#ping'
@@ -58,5 +60,28 @@ Rails.application.routes.draw do
       resources :webhook, only: [:create]
       resources :strategies, only: [:show]
     end
+  end
+
+  namespace :debug do
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      # Protect against timing attacks:
+      # - See https://codahale.com/a-lesson-in-timing-attacks
+      # - See https://thisdata.com/blog/timing-attacks-against-string-comparison
+      # - Use & (do not use &&) so that it doesn't short circuit.
+      # - Use digests to stop length information leaking (see also
+      #   ActiveSupport::SecurityUtils.variable_size_secure_compare)
+      ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(username),
+        ::Digest::SHA256.hexdigest(
+          Rails.application.secrets.sidekiq_http_username
+        )
+      ) & ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(password),
+        ::Digest::SHA256.hexdigest(
+          Rails.application.secrets.sidekiq_http_password
+        )
+      )
+    end
+    mount Sidekiq::Web, at: '/sidekiq'
   end
 end
---
+++ api/spec/requests/sidekiq_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe 'Sidekiq dashboard', type: :request do
+  before do
+    allow(
+      Rails.application.secrets
+    ).to receive(:sidekiq_http_username).and_return('AzureDiamond')
+
+    allow(
+      Rails.application.secrets
+    ).to receive(:sidekiq_http_password).and_return('hunter2')
+  end
+
+  it 'should fail with no credentials' do
+    get '/debug/sidekiq'
+
+    expect(response.status).to eq(401)
+  end
+
+  it 'should fail with incorrect credentials' do
+    get '/debug/sidekiq', headers: {
+      'HTTP_AUTHORIZATION': auth_header('AzureDiamond', 'hunter3')
+    }
+
+    expect(response.status).to eq(401)
+  end
+
+  it 'should load with the right credentials' do
+    get '/debug/sidekiq', headers: {
+      'HTTP_AUTHORIZATION': auth_header('AzureDiamond', 'hunter2')
+    }
+
+    expect(response.status).to eq(200)
+  end
+
+  private
+
+  def auth_header(username, password)
+    ActionController::HttpAuthentication::Basic.encode_credentials(
+      username,
+      password
+    )
+  end
+end
