f4c5b920bf8ce307756f3da55d9ad1f15c1ea89d
Zach Latta <zach@zachlatta.com>
2018-03-19 15:02:48 -0700

    Make banners 500px wide instead of 750px

--- api/app/models/event_banner.rb
+++ api/app/models/event_banner.rb
@@ -17,7 +17,7 @@ class EventBanner < Attachment
       define: 'jpeg:dct-method=float',
       sampling_factor: '4:2:0',
       # resize
-      resize: '750x'
+      resize: '500x'
     )
   end
 end
