effbadbd39b486f06956fd20bd7684df9aa04887
Zach Latta <zach@zachlatta.com>
2018-05-15 18:29:02 -0700

    Use cached upvote_count

--- app/models/challenge_post.rb
+++ app/models/challenge_post.rb
@@ -46,6 +46,6 @@ class ChallengePost < ApplicationRecord
     gravity = 1.8 # adjust as necessary
     hours_since_creation = (Time.current - created_at) / 1.hour
 
-    (upvotes.count + 1) / (hours_since_creation + 2)**gravity
+    (upvote_count + 1) / (hours_since_creation + 2)**gravity
   end
 end
