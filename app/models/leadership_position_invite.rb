969373d38731b9b77026adf582a416b467b02a6c
Zach Latta <zach@zachlatta.com>
2018-05-22 09:16:37 -0700

    LeadershipPositionInvite belongs_to LeadershipPosition

--- app/models/leadership_position_invite.rb
+++ app/models/leadership_position_invite.rb
@@ -4,6 +4,7 @@ class LeadershipPositionInvite < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :new_club
   belongs_to :user
+  belongs_to :leadership_position
 
   validates :sender, :new_club, :user, presence: true
   validates :accepted_at, absence: true, if: -> { rejected_at.present? }
