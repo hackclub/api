8820f034abe3535a706724ed6f829171cf13ccdd
Max Wofford <max@maxwofford.com>
2019-03-15 00:51:03 -0700

    Rubocop

--- app/jobs/send_application_to_airtable_job.rb
+++ app/jobs/send_application_to_airtable_job.rb
@@ -4,27 +4,27 @@ class SendApplicationToAirtableJob < ApplicationJob
   queue_as :default
 
   def perform(new_club_application)
-    club_record = Airtable::Record.new( "High School Name": new_club_application.high_school_name )
+    club_record = Airtable::Record.new("High School Name": new_club_application.high_school_name)
 
-    table("Clubs").create club_record
+    table('Clubs').create club_record
 
     poc = new_club_application.leader_profiles.find_by(user_id: new_club_application.point_of_contact_id)
     leader_record = Airtable::Record.new(
       "Full Name": poc.leader_name,
       "Email": poc.leader_email,
-      "Clubs": [ club_record.id ]
+      "Clubs": [club_record.id]
     )
 
-    table("Leaders").create leader_record
+    table('Leaders').create leader_record
 
     note_record = Airtable::Record.new(
-      "Type": [ "Application submitted", "Note" ],
-      "Club": [ club_record.id ],
+      "Type": ['Application submitted', 'Note'],
+      "Club": [club_record.id],
       "Date": Date.today.to_s,
-      "Notes": "Application submitted (and added to Airtable by API)"
+      "Notes": 'Application submitted (and added to Airtable by API)'
     )
 
-    table("History").create note_record
+    table('History').create note_record
   end
 
   private
